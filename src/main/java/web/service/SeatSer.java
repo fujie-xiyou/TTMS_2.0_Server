cc9a3001e4e853cad361d4bcea2379346f08498b
付杰 <fujie.me@qq.com>
2018-06-03 23:40:52 +0800

    好像没改什么啊

--- src/main/java/web/service/SeatSer.java
+++ src/main/java/web/service/SeatSer.java
@@ -1,16 +1,20 @@
 package web.service;
 
+import web.idao.DAOFactory;
+import web.idao.iSeatDAO;
+import web.idao.iStudioDAO;
 import web.model.Seat;
 
 import java.util.List;
 
 public class SeatSer {
+    iSeatDAO iSeatDAO = DAOFactory.creatSeatDAO();
     public int add(Seat seat){
-        return 1;
+        return iSeatDAO.insert(seat);
     }
     public int addAll(List<Seat> seats){
         //批量添加座位
-        return 1;
+        return iSeatDAO.insert(seats);
     }
     public int delete(Seat seat){
         return 0;
