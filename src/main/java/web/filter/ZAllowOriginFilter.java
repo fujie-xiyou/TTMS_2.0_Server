1ac644b516586a8eaab8397fd524990230f14d51
fujie <fujie@xiyoulinux.org>
2018-10-28 19:25:03 +0800

    允许ajax跨域请求

---
+++ src/main/java/web/filter/ZAllowOriginFilter.java
@@ -0,0 +1,34 @@
+package web.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//对所有请求允许JS跨域请求
+@WebFilter(filterName = "ZAllowOriginFilter",value = {"/*"})
+public class ZAllowOriginFilter implements Filter {
+    @Override
+    public void destroy() {
+    }
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("P3P", "CP=CAO PSA OUR");
+        if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
+            response.addHeader("Access-Control-Allow-Methods", "POST,GET,TRACE,OPTIONS");
+            response.addHeader("Access-Control-Allow-Headers", "Content-Type,Origin,Accept");
+            response.addHeader("Access-Control-Max-Age", "120");
+        }
+        chain.doFilter(req, resp);
+    }
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
